6a3722bd2cf8882afbf8a2ed7b1a1bf2439580f7
Rituraj Gupta <rituraj.iter@gmail.com>
2021-10-04 11:03:54 +0530

    Update p3.java

--- p3.java
+++ p3.java
@@ -1,11 +1,6 @@
-package coding_wizard_week9;
-
 import java.util.Scanner;
-
-public class p3
-{
- public static void main(String[] args)
- {
+public class p3{
+ public static void main(String[] args){
 	 Scanner sc = new Scanner(System.in);
 		int d_no[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 		String sy_roman[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
