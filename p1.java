7a1ecd515309c74a3ec5791fd9d11723fe436016
Rituraj Gupta <rituraj.iter@gmail.com>
2021-10-04 10:56:34 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,17 +1,9 @@
-package coding_wizard_week9;
-
 import java.util.Scanner;
-
 public class p1{ 
-
-	boolean find3Numbers(int A[], int arr_size, int sum) 
-	{ 
+	boolean find3Numbers(int A[], int arr_size, int sum) { 
 		int l, r; 
-
 		for (int i = 0; i < arr_size - 2; i++) { 
-			
 			for (int j = i + 1; j < arr_size - 1; j++) { 
-
 				for (int k = j + 1; k < arr_size; k++) { 
 					if (A[i] + A[j] + A[k] == sum) { 
 						System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]); 
@@ -23,15 +15,11 @@ public class p1{
 
 		return false; 
 	} 
-    public static void main(String[] args) 
-	{ 
+    public static void main(String[] args) { 
 		p1 triplet = new p1(); 
 		int A[] = { 12,3,4,1,6,9}; 
-		//int A[] = { 1,2,3,4,5}; 
 		int sum = 24; 
-		//int sum=9;
 		int arr_size = A.length; 
-
 		triplet.find3Numbers(A, arr_size, sum); 
 	} 
-} 
+}
